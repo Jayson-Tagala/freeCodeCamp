fc278832473ccbb0c7241f63268f5f1b3ac68b0a
Jayson Tagala <121615684+Jayson-Tagala@users.noreply.github.com>
2023-11-07 01:06:13 +0800

    JavaScript DOM Part 1
    At this night i've done stuying the basic components of DOM

---
+++ JavaScript DOM Manipulation Part I/Creating an Element/script.js
@@ -0,0 +1,32 @@
+// DOM Manipulation
+
+// Creating Elements
+
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+// Adding Elements
+ul.append(li);
+// Modify the text
+
+const firstListItem = document.querySelector(".list-items");
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.textContent);
+// console.log(firstListItem.innerHTML);
+//
+
+// li.innerText = "X-men";
+
+// Modifying Attributes & Classes
+
+// li.setAttribute("id", "main-heading");
+// li.removeAttribute("id");
+
+// const title = document.querySelector("main-heading");
+
+// console.log(title.getAttribute("id"));
+
+// li.classList.add("list-items");
+
+// Remove Elements
+
+// li.remove();
